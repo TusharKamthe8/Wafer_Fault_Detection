f7f0659fcf548089ee8ce05e65e8b411bfedac84
TusharKamthe8 <tushar.kamthe.ds@ghrce.raisoni.net>
2023-07-20 14:26:56 +0530

    first commit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="src",
     version="0.0.1",
-    author="iNeuron",
-    author_email="cloud@ineuron.ai",
+    author="TusharKamthe",
+    author_email="tusharkamthe34@gmail.com",
     packages=find_packages(),
     install_requires=[],
 )
